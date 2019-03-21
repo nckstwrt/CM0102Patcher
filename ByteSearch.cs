9c6b536a698983e789703b0740c2befaeef1fe2f
nckstwrt <48511757+nckstwrt@users.noreply.github.com>
2019-03-21 09:34:55 +0000

    RGNConverter sped up by not using Get/SetPixel Added a ByteSearcher/Replacer class

---
+++ ByteSearch.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CM0102Patcher
+{
+    public class ByteSearch
+    {
+        public static byte[] LoadFile(string file)
+        {
+            using (var fs = File.OpenRead(file))
+            {
+                using (var br = new BinaryReader(fs))
+                {
+                    return br.ReadBytes((int)br.BaseStream.Length);
+                }
+            }
+        }
+
+        public static int SearchBytes(byte[] toSearch, byte[] searchBytes, int startIndex = 0)
+        {
+            int ptr = 0;
+            for (int i = startIndex; i < toSearch.Length; i++)
+            {
+                if (toSearch[i] == searchBytes[ptr])
+                {
+                    ptr++;
+                    if (ptr == searchBytes.Length)
+                        return (i - searchBytes.Length) + 1;
+                }
+                else
+                {
+                    if (ptr > 0)
+                    {
+                        i -= ptr;
+                        ptr = 0;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public static List<int> SearchBytesForAll(byte[] toSearch, byte[] searchBytes)
+        {
+            List<int> offsets = new List<int>();
+            int idx = 0;
+            while (true)
+            {
+                var found = SearchBytes(toSearch, searchBytes, idx);
+                if (found != -1)
+                {
+                    offsets.Add(found);
+                    idx = found + 1;
+                }
+                else
+                    break;
+            }
+            return offsets;
+        }
+
+        public static byte[] StringToBytePad(string s, int padTo = 0)
+        {
+            Encoding latin1 = Encoding.GetEncoding("ISO-8859-1");
+            var stringBytes = latin1.GetBytes(s);
+            if (padTo > 0)
+            {
+                var newString = new byte[padTo];
+                stringBytes.CopyTo(newString, 0);
+                stringBytes = newString;
+            }
+            return stringBytes;
+        }
+
+        public static void WriteToBlock(byte[] block, int idx, string str, int padTo = 0)
+        {
+            var strBytes = StringToBytePad(str, padTo);
+            strBytes.CopyTo(block, idx);
+        }
+
+        public static void WriteToBinaryWriter(BinaryWriter bw, int pos, string str, int padTo = 0)
+        {
+            var strBytes = StringToBytePad(str, padTo);
+            bw.BaseStream.Seek(pos, SeekOrigin.Begin);
+            bw.Write(strBytes);
+        }
+
+        public static void WriteToFile(string file, int pos, string str, int padTo = 0)
+        {
+            File.SetAttributes(file, FileAttributes.Normal);
+            using (var fs = File.Open(file, FileMode.Open, FileAccess.Write))
+            {
+                using (var bw = new BinaryWriter(fs))
+                {
+                    WriteToBinaryWriter(bw, pos, str, padTo);
+                }
+            }
+        }
+
+        public static void TextFileReplace(string file, string toReplace, string replaceWith)
+        {
+            File.SetAttributes(file, FileAttributes.Normal);
+            using (var fs = File.Open(file, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (var tr = new StreamReader(fs))
+                {
+                    using (var tw = new StreamWriter(fs))
+                    {
+                        var text = tr.ReadToEnd();
+                        text = text.Replace(toReplace, replaceWith);
+                        fs.SetLength(0);
+                        tw.Write(text);
+                    }
+                }
+            }
+        }
+    }
+}
--- RGNConverter.cs
+++ RGNConverter.cs
@@ -73,21 +73,28 @@ namespace CM0102Patcher
                 var height = br.ReadUInt32();
                 var bmp = new Bitmap((int)width, (int)height);
                 br.BaseStream.Seek(40, SeekOrigin.Current);
+                var bmpBits = bmp.LockBits(new Rectangle(0, 0, (int)width, (int)height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+                var bytes = new byte[bmpBits.Stride * bmpBits.Height];
+                int ptrStart = 0;
+                int ptr = 0;
                 for (int y = 0; y < height; y++)
                 {
+                    ptr = ptrStart;
                     for (int x = 0; x < width; x++)
                     {
                         var rgb16 = br.ReadUInt16();
                         var _5bitRedChannel = (rgb16 & 0xF800) >> 11;
                         var _6bitGreenChannel = (rgb16 & 0x7E0) >> 5;
                         var _5bitBlueChannel = (rgb16 & 0x1f);
-                        int red = (_5bitRedChannel * 255 + 15) / 31;
-                        int green = (_6bitGreenChannel * 255 + 31) / 63;
-                        int blue = (_5bitBlueChannel * 255 + 15) / 31;
 
-                        bmp.SetPixel(x, y, Color.FromArgb(red, green, blue));
+                        bytes[ptr++] = (byte)((_5bitBlueChannel * 255 + 15) / 31);
+                        bytes[ptr++] = (byte)((_6bitGreenChannel * 255 + 31) / 63);
+                        bytes[ptr++] = (byte)((_5bitRedChannel * 255 + 15) / 31);
                     }
+                    ptrStart += bmpBits.Stride;
                 }
+                System.Runtime.InteropServices.Marshal.Copy(bytes, 0, bmpBits.Scan0, bytes.Length);
+                bmp.UnlockBits(bmpBits);
                 if (newWidth == -1 && newHeight == -1)
                 {
                     return bmp;
@@ -126,17 +133,26 @@ namespace CM0102Patcher
                     string byteValue = header.Substring(i, 2);
                     rgnFile.Write(byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture));
                 }
+
+                var bmpBits = bmp.LockBits(new Rectangle(0, 0, (int)width, (int)height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                byte[] bytes = new byte[bmpBits.Stride * bmpBits.Height];
+                System.Runtime.InteropServices.Marshal.Copy(bmpBits.Scan0, bytes, 0, bytes.Length);
+                bmp.UnlockBits(bmpBits);
+
+                int ptrStart = 0;
+                int ptr = 0;
                 for (int y = 0; y < height; y++)
                 {
+                    ptr = ptrStart;
                     for (int x = 0; x < width; x++)
                     {
-                        var col = bmp.GetPixel(x, y);
-                        UInt32 red = (UInt32)(col.R * 31 + 127) / 255;
-                        UInt32 green = (UInt32)(col.G * 63 + 127) / 255;
-                        UInt32 blue = (UInt32)(col.B * 31 + 127) / 255;
+                        UInt32 blue = (UInt32)(bytes[ptr++] * 31 + 127) / 255;
+                        UInt32 green = (UInt32)(bytes[ptr++] * 63 + 127) / 255;
+                        UInt32 red = (UInt32)(bytes[ptr++] * 31 + 127) / 255;
                         UInt16 RGB565pixel = (UInt16)((red << 11) | (green << 5) | blue);
                         rgnFile.Write(RGB565pixel);
                     }
+                    ptrStart += bmpBits.Stride;
                 }
             }
         }
