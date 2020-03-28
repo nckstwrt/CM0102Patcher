f499c70c43d9d218f489a8050831c66a245bf58a
nckstwrt <48511757+nckstwrt@users.noreply.github.com>
2020-03-28 15:42:25 +0000

    First cut of SaveReader2 Code

---
+++ SaveChanger/SaveReader2.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CM0102Patcher
+{
+    public class SaveReader2
+    {
+        public void Load(string inFile)
+        {
+            using (var fin = File.OpenRead(inFile))
+            using (var br = new BinaryReader(fin))
+            {
+                var compressed = (br.ReadInt32() == 4);
+
+                // Skip 4 bytes
+                unknownHdrBytes = br.ReadInt32();
+
+                // Read Blocks
+                var blockCount = br.ReadInt32();
+                for (int i = 0; i < blockCount; i++)
+                {
+                    var block = new Block();
+                    block.blockBuffer = br.ReadBytes(268);
+                    Console.WriteLine("{0}", block.Name);
+                    blocks.Add(block);
+                }
+
+                // Read Data
+                foreach (var block in blocks)
+                {
+                    fin.Seek(block.Position, SeekOrigin.Begin);
+                    block.dataBuffer = new byte[block.Size];
+
+                    int bufPtr = 0;
+                    while (bufPtr < block.Size)
+                    {
+                        var b = fin.ReadByte();
+                        if (!compressed || b <= 128)
+                        {
+                            block.dataBuffer[bufPtr++] = (byte)b;
+                        }
+                        else
+                        {
+                            byte byteCount = (byte)(b - 128);
+                            byte actualByte = (byte)fin.ReadByte();
+                            for (byte i = 0; i < byteCount; i++)
+                                block.dataBuffer[bufPtr++] = (byte)actualByte;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Block FindBlock(string blockName)
+        {
+            return blocks.FirstOrDefault(x => x.Name == blockName);
+        }
+
+        public void DumpBlockToFile(string blockName, string fileName)
+        {
+            var block = FindBlock(blockName);
+            using (var fout = File.Create(fileName))
+                fout.Write(block.dataBuffer, 0, block.dataBuffer.Length);
+        }
+
+        public List<T> BlockToObjects<T>(string blockName, bool contractSpecific = false)
+        {
+            List<T> ret = null;
+            var block = FindBlock(blockName);
+            if (block != null)
+            {
+                int startAt = 0;
+                if (contractSpecific)
+                {
+                    var intPreCount = BitConverter.ToInt32(block.dataBuffer, 0);
+                    var blockCount = BitConverter.ToInt32(block.dataBuffer, 4);
+                    startAt = (8 + intPreCount * 21);
+                }
+
+                var slices = SliceBlock(block, Marshal.SizeOf(typeof(T)), startAt);
+                ret = CastSlicesToObjects<T>(slices);
+            }
+            return ret;
+        }
+
+        public void ObjectsToBlock<T>(string blockName, List<T> objects, bool contractSpecific = false)
+        {
+            var block = FindBlock(blockName);
+            if (block != null)
+            {
+                var slices = CastObjectsToSlices(objects);
+                Rebuild(block, slices, contractSpecific);
+            }
+        }
+
+        public List<string> NamesFromBlock(string blockName)
+        {
+            List<string> ret = null;
+            var block = FindBlock(blockName);
+            if (block != null)
+            {
+                ret = new List<string>();
+                var slices = SliceBlock(block, 60);
+                foreach (var slice in slices)
+                    ret.Add(latin1.GetString(slice, 0, 50).TrimEnd('\0'));
+            }
+            return ret;
+        }
+
+        public DateTime GetCurrentGameDate()
+        {
+            var block = FindBlock("general.dat");
+            return TCMDate.ToDateTime(new TCMDate(block.dataBuffer, 3944));
+        }
+
+        public TStaff FindPlayer(string firstName, string lastName, List<TStaff> staff, bool ignoreSymbols = false)
+        {
+            TStaff ret = new TStaff();
+            if (firstNames == null)
+            {
+                firstNames = NamesFromBlock("first_names.dat");
+                secondNames = NamesFromBlock("second_names.dat");
+            }
+
+            int fname_idx = -1;
+            int sname_idx = -1;
+            if (ignoreSymbols)
+            {
+                for (int i = 0; i < firstNames.Count(); i++)
+                {
+                    if (string.Compare(firstName, firstNames[i], CultureInfo.CurrentCulture, CompareOptions.IgnoreNonSpace) == 0)
+                    {
+                        fname_idx = i;
+                        break;
+                    }
+                }
+                for (int i = 0; i < secondNames.Count(); i++)
+                {
+                    if (string.Compare(lastName, secondNames[i], CultureInfo.CurrentCulture, CompareOptions.IgnoreNonSpace) == 0)
+                    {
+                        sname_idx = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                fname_idx = firstNames.IndexOf(firstName);
+                sname_idx = secondNames.IndexOf(lastName);
+            }
+
+            if (fname_idx != -1 && sname_idx != -1)
+            {
+                ret = staff.FirstOrDefault(x => x.FirstName == fname_idx && x.SecondName == sname_idx);
+            }
+
+            return ret;
+        }
+
+        List<byte[]> SliceBlock(Block block, int sliceSize, int startAt = 0)
+        {
+            List<byte[]> slices = new List<byte[]>();
+            for (int i = startAt; i < block.dataBuffer.Length; i += sliceSize)
+            {
+                var slice = new byte[sliceSize];
+                if (i + sliceSize <= block.dataBuffer.Length)
+                    Array.Copy(block.dataBuffer, i, slice, 0, sliceSize);
+                slices.Add(slice);
+            }
+            return slices;
+        }
+
+        List<T> CastSlicesToObjects<T>(List<byte[]> slices)
+        {
+            var ret = new List<T>();
+            foreach (var slice in slices)
+            {
+                int objSize = Marshal.SizeOf(typeof(T));
+                var ptrObj = Marshal.AllocHGlobal(objSize);
+                Marshal.Copy(slice, 0, ptrObj, objSize);
+                var obj = (T)Marshal.PtrToStructure(ptrObj, typeof(T));
+                ret.Add(obj);
+                Marshal.FreeHGlobal(ptrObj);
+            }
+            return ret;
+        }
+
+        List<byte[]> CastObjectsToSlices<T>(List<T> objects)
+        {
+            var ret = new List<byte[]>();
+            foreach (var objbect in objects)
+            {
+                int objSize = Marshal.SizeOf(typeof(T));
+                byte[] arr = new byte[objSize];
+                IntPtr ptr = Marshal.AllocHGlobal(objSize);
+                Marshal.StructureToPtr(objbect, ptr, true);
+                Marshal.Copy(ptr, arr, 0, objSize);
+                Marshal.FreeHGlobal(ptr);
+                ret.Add(arr);
+            }
+            return ret;
+        }
+
+        void Rebuild(Block block, List<byte[]> slices, bool contractSpecific = false)
+        {
+            var totalSliceSize = slices.Sum(x => x.Length);
+            byte[] newDataBuffer;
+            int ptr = 0;
+
+            if (contractSpecific)
+            {
+                var intPreCount = BitConverter.ToInt32(block.dataBuffer, 0);
+                ptr = (8 + intPreCount * 21);
+                newDataBuffer = new byte[totalSliceSize + ptr + 13];
+                Array.Copy(block.dataBuffer, 0, newDataBuffer, 0, ptr);
+
+                // Write new Block Count
+                Array.Copy(BitConverter.GetBytes(slices.Count), 0, newDataBuffer, 4, 4);
+
+                newDataBuffer[newDataBuffer.Length - 1] = 1;
+            }
+            else
+                newDataBuffer = new byte[totalSliceSize];
+
+            foreach (var slice in slices)
+            {
+                Array.Copy(slice, 0, newDataBuffer, ptr, slice.Length);
+                ptr += slice.Length;
+            }
+
+            block.dataBuffer = newDataBuffer;
+        }
+
+        public void Write(string outFile, bool compressed)
+        {
+            using (var fout = File.Create(outFile))
+            using (var bw = new BinaryWriter(fout))
+            {
+                // 3 is uncompressed, 4 is compressed (swap depending on what you're doing)
+                bw.Write((int)(compressed ? 4 : 3));
+
+                // Unknown Bytes
+                bw.Write(unknownHdrBytes);
+
+                // Write Block Count
+                bw.Write(blocks.Count);
+
+                // Skip to go write the data
+                fout.Seek((blocks.Count * 268) + 12, SeekOrigin.Begin);
+
+                // Write Block Data
+                foreach (var block in blocks)
+                {
+                    block.Position = (uint)fout.Position;
+                    block.Size = (uint)block.dataBuffer.Length;
+                    bw.Write(compressed ? Compress(block.dataBuffer) : block.dataBuffer);
+                }
+
+                // Write Block Headers (with updated positions)
+                fout.Seek(12, SeekOrigin.Begin);
+                foreach (var block in blocks)
+                {
+                    bw.Write(block.blockBuffer);
+                }
+            }
+        }
+
+        byte[] Compress(byte[] buffer)
+        {
+            var ms = new MemoryStream();
+            int lastByte = -1;
+            int lastByteCount = 0;
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                var b = buffer[i];
+
+                if (lastByte == -1)
+                    lastByte = b;
+
+                if (b != lastByte || lastByteCount >= 126)
+                {
+                    if (lastByteCount == 1 && lastByte < 128)
+                        ms.WriteByte((byte)lastByte);
+                    else
+                    {
+                        ms.WriteByte((byte)(lastByteCount + 128));
+                        ms.WriteByte((byte)lastByte);
+                    }
+
+                    lastByte = b;
+                    lastByteCount = 1;
+                }
+                else
+                {
+                    lastByteCount++;
+                }
+            }
+
+            // Write last part
+            if (lastByteCount == 1 && lastByte < 128)
+                ms.WriteByte((byte)lastByte);
+            else
+            {
+                ms.WriteByte((byte)(lastByteCount + 128));
+                for (int j = 0; j < lastByteCount; j++)
+                    ms.WriteByte((byte)lastByte);
+            }
+
+            return ms.ToArray();
+        }
+
+        int unknownHdrBytes = 0x16;
+        Encoding latin1 = Encoding.GetEncoding("ISO-8859-1");
+        List<Block> blocks = new List<Block>();
+        List<string> firstNames = null;
+        List<string> secondNames = null;
+    }
+}
---
+++ SaveChanger/Structures.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CM0102Patcher
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TCMDate
+    {
+        public short Day;
+        public short Year;
+        public int LeapYear;
+
+        public TCMDate(byte[] data, int position)
+        {
+            Day = BitConverter.ToInt16(data, position);
+            Year = BitConverter.ToInt16(data, position + 2);
+            LeapYear = BitConverter.ToInt16(data, position + 4);
+        }
+
+        public static TCMDate FromDateTime(DateTime dt)
+        {
+            TCMDate date = new TCMDate();
+            date.Day = (short)(dt.DayOfYear - 1);
+            date.Year = (short)dt.Year;
+            date.LeapYear = DateTime.IsLeapYear(date.Year) ? 1 : 0;
+            return date;
+        }
+
+        public static DateTime ToDateTime(TCMDate date)
+        {
+            return new DateTime(date.Year, 1, 1).AddDays(date.Day);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TContract
+    {
+        public int ID;
+        public int Club;
+        public int Unknown;
+        public int Wage;
+        public int GoalBonus;
+        public int AssistBonus;
+        public int CleanSheetBonus;
+        public byte NonPromotionRC;
+        public byte MinimumFeeRC;
+        public byte NonPlayingRC;
+        public byte RelegationRC;
+        public byte ManagerJobRC;
+        public int ReleaseFee;
+        public TCMDate DateStarted;
+        public TCMDate ContractExpires;
+        public byte ContractType;
+        public UInt64 Unknown18_1;
+        public UInt64 Unknown18_2;
+        public short Unknown18_3;
+        public byte Unknown18_4;
+        public byte LeavingOnBosman;
+        public int TransferArrangedFor;
+        public byte TransferStatus;
+        public byte SquadStatus;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TStaff
+    {
+        public int ID;
+        public int FirstName;
+        public int SecondName;
+        public int CommonName;
+        public TCMDate DateOfBirth;
+        public UInt16 YearOfBirth;
+        public int Nation;
+        public int SecondNation;
+        public byte IntApps;
+        public byte IntGoals;
+        public int NationalJob;
+        public byte JobForNation;
+        public TCMDate DateJoinedNation;
+        public TCMDate DateExpiresNation;
+        public int ClubJob;
+        public byte JobForClub;
+        public TCMDate DateJoinedClub;
+        public TCMDate DateExpiresClub;
+        public int Wage;
+        public int Value;
+        public byte Adaptability;
+        public byte Ambition;
+        public byte Determination;
+        public byte Loyality;
+        public byte Pressure;
+        public byte Professionalism;
+        public byte Sportsmanship;
+        public byte Temperament;
+        public byte PlayingSquad;
+        public byte Classification;
+        public byte ClubValuation;
+        public int Player;
+        public int StaffPreferences;
+        public int NonPlayer;
+        public byte SquadSelectedFor;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TPlayer
+    {
+        public int ID;
+        public byte SquadNumber;
+        public UInt16 CurrentAbility;
+        public UInt16 PotentialAbility;
+        public UInt16 HomeReputation;
+        public UInt16 CurrentReputation;
+        public UInt16 WorldReputation;
+        public sbyte Goalkeeper;
+        public sbyte Sweeper;
+        public sbyte Defender;
+        public sbyte DefensiveMidfielder;
+        public sbyte Midfielder;
+        public sbyte AttackingMidfielder;
+        public sbyte Attacker;
+        public sbyte WingBack;
+        public sbyte RightSide;
+        public sbyte LeftSide;
+        public sbyte Central;
+        public sbyte FreeRole;
+        public sbyte Acceleration;
+        public sbyte Aggression;
+        public sbyte Agility;
+        public sbyte Anticipation;
+        public sbyte Balance;
+        public sbyte Bravery;
+        public sbyte Consistency;
+        public sbyte Corners;
+        public sbyte Crossing;
+        public sbyte Decisions;
+        public sbyte Dirtiness;
+        public sbyte Dribbling;
+        public sbyte Finishing;
+        public sbyte Flair;
+        public sbyte FreeKicks;
+        public sbyte Handling;
+        public sbyte Heading;
+        public sbyte ImportantMatches;
+        public sbyte InjuryProneness;
+        public sbyte Jumping;
+        public sbyte Leadership;
+        public sbyte LeftFoot;
+        public sbyte LongShots;
+        public sbyte Marking;
+        public sbyte Movement;
+        public sbyte NaturalFitness;
+        public sbyte OneOnOnes;
+        public sbyte PlayerPace;
+        public sbyte Passing;
+        public sbyte Penalties;
+        public sbyte Positioning;
+        public sbyte Reflexes;
+        public sbyte RightFoot;
+        public sbyte Stamina;
+        public sbyte Strength;
+        public sbyte Tackling;
+        public sbyte Teamwork;
+        public sbyte Technique;
+        public sbyte ThrowIns;
+        public sbyte Versatility;
+        public sbyte Vision;
+        public sbyte WorkRate;
+        public byte PlayerMorale;
+    }
+
+    public class Block
+    {
+        public byte[] blockBuffer;
+        public byte[] dataBuffer;
+        public uint Position
+        {
+            get
+            {
+                return BitConverter.ToUInt32(blockBuffer, 0);
+            }
+            set
+            {
+                Array.Copy(BitConverter.GetBytes(value), 0, blockBuffer, 0, 4);
+            }
+        }
+
+        public uint Size
+        {
+            get
+            {
+                return BitConverter.ToUInt32(blockBuffer, 4);
+            }
+            set
+            {
+                Array.Copy(BitConverter.GetBytes(value), 0, blockBuffer, 4, 4);
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                int idx = Array.IndexOf(blockBuffer, (byte)0, 8);
+                return Encoding.ASCII.GetString(blockBuffer, 8, idx - 8);
+            }
+        }
+    }
+}
