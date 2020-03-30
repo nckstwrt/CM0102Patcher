ea02c4fdf4ece144601e8d3ffe1c30092322bfda
nckstwrt <48511757+nckstwrt@users.noreply.github.com>
2020-03-30 18:22:32 +0100

    Up'd version

--- PatcherForm.Designer.cs
+++ PatcherForm.Designer.cs
@@ -654,7 +654,7 @@
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "PatcherForm";
-            this.Text = "Nick\'s CM0102Patcher v1.26";
+            this.Text = "Nick\'s CM0102Patcher v1.27";
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PatcherForm_KeyPress);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.numericGameStartYear)).EndInit();
