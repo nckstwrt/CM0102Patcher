7854691a59f6e44d2922b25e1cf61ed0bc118534
nckstwrt <nick.g.stewart@gmail.com>
2020-06-30 18:45:44 +0100

    Some structural changes to support remove accents from names when required.

--- History Editor/HistoryLoader.cs
+++ History Editor/HistoryLoader.cs
@@ -29,57 +29,6 @@ namespace CM0102Patcher
 
         Encoding latin1 = Encoding.GetEncoding("ISO-8859-1");
 
-        public List<T> ReadFile<T>(string fileName, int seekTo = 0, int count = 0)
-        {
-            List<T> ret = new List<T>();
-
-            using (var fin = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
-            using (var br = new BinaryReader(fin))
-            {
-                fin.Seek(seekTo, SeekOrigin.Begin);
-                int objSize = Marshal.SizeOf(typeof(T));
-
-                int counter = 0;
-                while (true)
-                {
-                    var bytes = br.ReadBytes(objSize);
-                    if (count != 0 && counter == count)
-                        break;
-                    if (bytes == null || bytes.Length != objSize)
-                        break;
-                    var ptrObj = Marshal.AllocHGlobal(objSize);
-                    Marshal.Copy(bytes, 0, ptrObj, objSize);
-                    var obj = (T)Marshal.PtrToStructure(ptrObj, typeof(T));
-                    ret.Add(obj);
-                    Marshal.FreeHGlobal(ptrObj);
-                    counter++;
-                }
-            }
-            
-            return ret;
-        }
-
-        public void SaveFile<T>(string fileName, List<T> data, int seekTo = 0)
-        {
-            using (var fout = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
-            using (var bw = new BinaryWriter(fout))
-            {
-                int objSize = Marshal.SizeOf(typeof(T));
-                // fout.SetLength(seekTo);
-                fout.Seek(seekTo, SeekOrigin.Begin);
-
-                foreach (var obj in data)
-                {
-                    byte[] arr = new byte[objSize];
-                    IntPtr ptr = Marshal.AllocHGlobal(objSize);
-                    Marshal.StructureToPtr(obj, ptr, true);
-                    Marshal.Copy(ptr, arr, 0, objSize);
-                    Marshal.FreeHGlobal(ptr);
-                    bw.Write(arr);
-                }
-            }
-        }
-
         public string GetTextFromBytes(byte[] bytes)
         {
             var ret = "";
@@ -95,17 +44,17 @@ namespace CM0102Patcher
         {
             var dir = Path.GetDirectoryName(indexFile);
             
-            index = ReadFile<TIndex>(indexFile, 8);
-            nation_comp = ReadFile<TComp>(Path.Combine(dir, "nation_comp.dat"));
-            club_comp = ReadFile<TComp>(Path.Combine(dir, "club_comp.dat"));
-            club = ReadFile<TClub>(Path.Combine(dir, "club.dat"));
-            nat_club = ReadFile<TClub>(Path.Combine(dir, "nat_club.dat"));
-            nation = ReadFile<TNation>(Path.Combine(dir, "nation.dat"));
-            nation_comp_history = ReadFile<TCompHistory>(Path.Combine(dir, "nation_comp_history.dat"));
-            club_comp_history = ReadFile<TCompHistory>(Path.Combine(dir, "club_comp_history.dat"));
+            index = MiscFunctions.ReadFile<TIndex>(indexFile, 8);
+            nation_comp = MiscFunctions.ReadFile<TComp>(Path.Combine(dir, "nation_comp.dat"));
+            club_comp = MiscFunctions.ReadFile<TComp>(Path.Combine(dir, "club_comp.dat"));
+            club = MiscFunctions.ReadFile<TClub>(Path.Combine(dir, "club.dat"));
+            nat_club = MiscFunctions.ReadFile<TClub>(Path.Combine(dir, "nat_club.dat"));
+            nation = MiscFunctions.ReadFile<TNation>(Path.Combine(dir, "nation.dat"));
+            nation_comp_history = MiscFunctions.ReadFile<TCompHistory>(Path.Combine(dir, "nation_comp_history.dat"));
+            club_comp_history = MiscFunctions.ReadFile<TCompHistory>(Path.Combine(dir, "club_comp_history.dat"));
             
-            staff_comp = ReadFile<TStaffComp>(Path.Combine(dir, "staff_comp.dat"));
-            staff_comp_history = ReadFile<TStaffCompHistory>(Path.Combine(dir, "staff_comp_history.dat"));
+            staff_comp = MiscFunctions.ReadFile<TStaffComp>(Path.Combine(dir, "staff_comp.dat"));
+            staff_comp_history = MiscFunctions.ReadFile<TStaffCompHistory>(Path.Combine(dir, "staff_comp_history.dat"));
 
             staffDetails = index.Find(x => GetTextFromBytes(x.Name) == "staff.dat" && x.FileType == 6);
 
@@ -114,12 +63,12 @@ namespace CM0102Patcher
                 throw new Exception("This is a very old version of the data!\r\n\r\nLoad in the Champ Man Editor and then save it to update it before history editing!\r\n\r\n");
             }
 
-            staff = ReadFile<TStaff>(Path.Combine(dir, "staff.dat"), staffDetails.Offset, staffDetails.Count);
-            staff_history = ReadFile<TStaffHistory>(Path.Combine(dir, "staff_history.dat"));
+            staff = MiscFunctions.ReadFile<TStaff>(Path.Combine(dir, "staff.dat"), staffDetails.Offset, staffDetails.Count);
+            staff_history = MiscFunctions.ReadFile<TStaffHistory>(Path.Combine(dir, "staff_history.dat"));
 
-            first_names = ReadFile<TNames>(Path.Combine(dir, "first_names.dat"));
-            second_names = ReadFile<TNames>(Path.Combine(dir, "second_names.dat"));
-            common_names = ReadFile<TNames>(Path.Combine(dir, "common_names.dat"));
+            first_names = MiscFunctions.ReadFile<TNames>(Path.Combine(dir, "first_names.dat"));
+            second_names = MiscFunctions.ReadFile<TNames>(Path.Combine(dir, "second_names.dat"));
+            common_names = MiscFunctions.ReadFile<TNames>(Path.Combine(dir, "common_names.dat"));
 
             staffNames = new Dictionary<int, string>();
             foreach (var staffMember in staff)
@@ -162,28 +111,28 @@ namespace CM0102Patcher
             UpdateIndex("staff_comp_history.dat", staff_comp_history);
             UpdateIndex("staff_history.dat", staff_history);
 
-            SaveFile<TIndex>(indexFile, index, 8);
+            MiscFunctions.SaveFile<TIndex>(indexFile, index, 8);
 
             if (saveClubData)
             {
-                SaveFile<TComp>(Path.Combine(dir, "club_comp.dat"), club_comp);
-                SaveFile<TClub>(Path.Combine(dir, "club.dat"), club);
+                MiscFunctions.SaveFile<TComp>(Path.Combine(dir, "club_comp.dat"), club_comp);
+                MiscFunctions.SaveFile<TClub>(Path.Combine(dir, "club.dat"), club);
             }
 
             if (saveStaffData)
             {
-                SaveFile<TStaff>(Path.Combine(dir, "staff.dat"), staff, staffDetails.Offset);
+                MiscFunctions.SaveFile<TStaff>(Path.Combine(dir, "staff.dat"), staff, staffDetails.Offset);
             }
 
             /*
-            SaveFile<TComp>(Path.Combine(dir, "nation_comp.dat"), nation_comp);
-            SaveFile<TClub>(Path.Combine(dir, "nat_club.dat"), nat_club);
-            SaveFile<TNation>(Path.Combine(dir, "nation.dat"), nation);
+            MiscFunctions.SaveFile<TComp>(Path.Combine(dir, "nation_comp.dat"), nation_comp);
+            MiscFunctions.SaveFile<TClub>(Path.Combine(dir, "nat_club.dat"), nat_club);
+            MiscFunctions.SaveFile<TNation>(Path.Combine(dir, "nation.dat"), nation);
             */
-            SaveFile<TCompHistory>(Path.Combine(dir, "nation_comp_history.dat"), nation_comp_history);
-            SaveFile<TCompHistory>(Path.Combine(dir, "club_comp_history.dat"), club_comp_history);
-            SaveFile<TStaffCompHistory>(Path.Combine(dir, "staff_comp_history.dat"), staff_comp_history);
-            SaveFile<TStaffHistory>(Path.Combine(dir, "staff_history.dat"), staff_history);
+            MiscFunctions.SaveFile<TCompHistory>(Path.Combine(dir, "nation_comp_history.dat"), nation_comp_history);
+            MiscFunctions.SaveFile<TCompHistory>(Path.Combine(dir, "club_comp_history.dat"), club_comp_history);
+            MiscFunctions.SaveFile<TStaffCompHistory>(Path.Combine(dir, "staff_comp_history.dat"), staff_comp_history);
+            MiscFunctions.SaveFile<TStaffHistory>(Path.Combine(dir, "staff_history.dat"), staff_history);
         }
 
         public void SortClubNames()
---
+++ MiscFunctions.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CM0102Patcher
+{
+    public static class MiscFunctions
+    {
+        static Encoding latin1 = Encoding.GetEncoding("ISO-8859-1");
+
+        public static string GetTextFromBytes(byte[] bytes)
+        {
+            var ret = "";
+            if (bytes != null)
+            {
+                int length = Array.IndexOf(bytes, (byte)0);
+                ret = latin1.GetString(bytes, 0, length);
+            }
+            return ret;
+        }
+
+        static Dictionary<string, string> foreign_characters = new Dictionary<string, string>
+        {
+            { "äæǽ", "ae" },
+            { "öœ", "oe" },
+            { "ü", "ue" },
+            { "Ä", "Ae" },
+            { "Ü", "Ue" },
+            { "Ö", "Oe" },
+            { "ÀÁÂÃÄÅǺĀĂĄǍΑΆẢẠẦẪẨẬẰẮẴẲẶА", "A" },
+            { "àáâãåǻāăąǎªαάảạầấẫẩậằắẵẳặа", "a" },
+            { "Б", "B" },
+            { "б", "b" },
+            { "ÇĆĈĊČ", "C" },
+            { "çćĉċč", "c" },
+            { "Д", "D" },
+            { "д", "d" },
+            { "ÐĎĐΔ", "Dj" },
+            { "ðďđδ", "dj" },
+            { "ÈÉÊËĒĔĖĘĚΕΈẼẺẸỀẾỄỂỆЕЭ", "E" },
+            { "èéêëēĕėęěέεẽẻẹềếễểệеэ", "e" },
+            { "Ф", "F" },
+            { "ф", "f" },
+            { "ĜĞĠĢΓГҐ", "G" },
+            { "ĝğġģγгґ", "g" },
+            { "ĤĦ", "H" },
+            { "ĥħ", "h" },
+            { "ÌÍÎÏĨĪĬǏĮİΗΉΊΙΪỈỊИЫ", "I" },
+            { "ìíîïĩīĭǐįıηήίιϊỉịиыї", "i" },
+            { "Ĵ", "J" },
+            { "ĵ", "j" },
+            { "ĶΚК", "K" },
+            { "ķκк", "k" },
+            { "ĹĻĽĿŁΛЛ", "L" },
+            { "ĺļľŀłλл", "l" },
+            { "М", "M" },
+            { "м", "m" },
+            { "ÑŃŅŇΝН", "N" },
+            { "ñńņňŉνн", "n" },
+            { "ÒÓÔÕŌŎǑŐƠØǾΟΌΩΏỎỌỒỐỖỔỘỜỚỠỞỢО", "O" },
+            { "òóôõōŏǒőơøǿºοόωώỏọồốỗổộờớỡởợо", "o" },
+            { "П", "P" },
+            { "п", "p" },
+            { "ŔŖŘΡР", "R" },
+            { "ŕŗřρр", "r" },
+            { "ŚŜŞȘŠΣС", "S" },
+            { "śŝşșšſσςс", "s" },
+            { "ȚŢŤŦτТ", "T" },
+            { "țţťŧт", "t" },
+            { "ÙÚÛŨŪŬŮŰŲƯǓǕǗǙǛŨỦỤỪỨỮỬỰУ", "U" },
+            { "ùúûũūŭůűųưǔǖǘǚǜυύϋủụừứữửựу", "u" },
+            { "ÝŸŶΥΎΫỲỸỶỴЙ", "Y" },
+            { "ýÿŷỳỹỷỵй", "y" },
+            { "В", "V" },
+            { "в", "v" },
+            { "Ŵ", "W" },
+            { "ŵ", "w" },
+            { "ŹŻŽΖЗ", "Z" },
+            { "źżžζз", "z" },
+            { "ÆǼ", "AE" },
+            { "ß", "ss" },
+            { "Ĳ", "IJ" },
+            { "ĳ", "ij" },
+            { "Œ", "OE" },
+            { "ƒ", "f" },
+            { "ξ", "ks" },
+            { "π", "p" },
+            { "β", "v" },
+            { "μ", "m" },
+            { "ψ", "ps" },
+            { "Ё", "Yo" },
+            { "ё", "yo" },
+            { "Є", "Ye" },
+            { "є", "ye" },
+            { "Ї", "Yi" },
+            { "Ж", "Zh" },
+            { "ж", "zh" },
+            { "Х", "Kh" },
+            { "х", "kh" },
+            { "Ц", "Ts" },
+            { "ц", "ts" },
+            { "Ч", "Ch" },
+            { "ч", "ch" },
+            { "Ш", "Sh" },
+            { "ш", "sh" },
+            { "Щ", "Shch" },
+            { "щ", "shch" },
+            { "ЪъЬь", "" },
+            { "Ю", "Yu" },
+            { "ю", "yu" },
+            { "Я", "Ya" },
+            { "я", "ya" },
+            {"\u05de", "th"}
+        };
+
+        public static char RemoveDiacritics(this char c)
+        {
+            foreach (KeyValuePair<string, string> entry in foreign_characters)
+            {
+                if (entry.Key.IndexOf(c) != -1)
+                {
+                    return entry.Value[0];
+                }
+            }
+            return c;
+        }
+
+        public static string RemoveDiacritics(this string s)
+        {
+            string text = "";
+            
+            foreach (char c in s)
+            {
+                int len = text.Length;
+
+                if (c == (char)5)
+                    Console.WriteLine();
+
+                foreach (KeyValuePair<string, string> entry in foreign_characters)
+                {
+                    if (entry.Key.IndexOf(c) != -1)
+                    {
+                        text += entry.Value;
+                        break;
+                    }
+                }
+
+                if (len == text.Length)
+                {
+                    text += c;
+                }
+            }
+            return text;
+        }
+
+        public static void RemoveAccentsFromNameFile(string fileName)
+        {
+            var names = ReadFile<TNames>(fileName);
+            for (int i = 0; i < names.Count; i++)
+            {
+                var name = GetTextFromBytes(names[i].Name);
+                if (names[i].Name[0] == 0xde)
+                    name = "Th" + name.Substring(1);
+                name = name.RemoveDiacritics();
+                Array.Copy(ASCIIEncoding.ASCII.GetBytes(name), names[i].Name, name.Length);
+            }
+            SaveFile(fileName, names);
+        }
+
+        public static List<T> ReadFile<T>(string fileName, int seekTo = 0, int count = 0)
+        {
+            List<T> ret = new List<T>();
+
+            using (var fin = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            using (var br = new BinaryReader(fin))
+            {
+                fin.Seek(seekTo, SeekOrigin.Begin);
+                int objSize = Marshal.SizeOf(typeof(T));
+
+                int counter = 0;
+                while (true)
+                {
+                    var bytes = br.ReadBytes(objSize);
+                    if (count != 0 && counter == count)
+                        break;
+                    if (bytes == null || bytes.Length != objSize)
+                        break;
+                    var ptrObj = Marshal.AllocHGlobal(objSize);
+                    Marshal.Copy(bytes, 0, ptrObj, objSize);
+                    var obj = (T)Marshal.PtrToStructure(ptrObj, typeof(T));
+                    ret.Add(obj);
+                    Marshal.FreeHGlobal(ptrObj);
+                    counter++;
+                }
+            }
+
+            return ret;
+        }
+
+        public static void SaveFile<T>(string fileName, List<T> data, int seekTo = 0)
+        {
+            using (var fout = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            using (var bw = new BinaryWriter(fout))
+            {
+                int objSize = Marshal.SizeOf(typeof(T));
+                // fout.SetLength(seekTo);
+                fout.Seek(seekTo, SeekOrigin.Begin);
+
+                foreach (var obj in data)
+                {
+                    byte[] arr = new byte[objSize];
+                    IntPtr ptr = Marshal.AllocHGlobal(objSize);
+                    Marshal.StructureToPtr(obj, ptr, true);
+                    Marshal.Copy(ptr, arr, 0, objSize);
+                    Marshal.FreeHGlobal(ptr);
+                    bw.Write(arr);
+                }
+            }
+        }
+    }
+}
