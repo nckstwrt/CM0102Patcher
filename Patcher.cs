3d78c5d561c883973690ce9c76ecf26788d188b6
nckstwrt <nick.g.stewart@gmail.com>
2021-01-02 10:46:32 +0000

    Another fix for DetectPatches

--- Patcher.cs
+++ Patcher.cs
@@ -164,7 +164,9 @@ namespace CM0102Patcher
                 bool matches = true;
                 foreach (var hexPatch in HexPatches)
                 {
-                    if (hexPatch.offset < fin.Length && hexPatch.offset != -1)
+                    if (hexPatch.offset != -1)
+                        continue;
+                    if (hexPatch.offset < fin.Length)
                     {
                         fin.Seek(hexPatch.offset, SeekOrigin.Begin);
                         byte[] patchBytes = HexStringToBytes(hexPatch.hex);
@@ -207,7 +209,7 @@ namespace CM0102Patcher
                     bool matches = true;
                     foreach (var hexPatch in patch.Value)
                     {
-                        if (hexPatch.offset == -1)
+                        if (hexPatch.offset != -1)
                             continue;
                         if (hexPatch.offset < fin.Length)
                         {
